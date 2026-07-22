0e1d5eee4c8e173f5adedb854369494dc1b60529
PoohBear2 <jzhou3379@gmail.com>
2026-07-22 14:19:55 -0400

    feat: implemented auth.ts for jwt token check with supabase to get userId

---
+++ lib/auth.ts
@@ -0,0 +1,40 @@
+import { createClient } from '@supabase/supabase-js';
+import { NextRequest } from 'next/server';
+
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseServiceRoleKey = process.env.SUPABSE_SERVICE_ROLE_KEY!;
+
+export async function getCliams(request: NextRequest): Promise<{ userId: string } | null> {
+	const token = request.headers.get('authorization');
+
+	if (!token) {
+		return null;
+	}
+
+	const supabase = createClient(supabaseUrl, supabaseServiceRoleKey, {
+		auth: {
+			autoRefreshToken: false,
+			persistSession: false
+		}
+	});
+
+	const { data, error } = await supabase.auth.getUser(token);
+
+	if (error || !data.user) {
+		return null;
+	}
+
+	return { userId: data.user.id };
+}
+
+export function unauthorized() {
+	return Response.json(
+		{
+			success: false,
+			error: 'unauthorized'
+		},
+		{
+			status: 401
+		}
+	);
+}
